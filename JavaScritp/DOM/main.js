893db3a285ba669397dbf766439ccdab34cdc0b3
Guadalupe Garcia <lupita_castellanos12@hotmail.com>
2020-08-16 18:46:12 -0500

    modificando y eliminando nodos en Javascritp

--- JavaScritp/DOM/main.js
+++ JavaScritp/DOM/main.js
@@ -6,11 +6,26 @@ var cajas=document.getElementsByTagName('div');
 
 
 //---Creando Nodos
-
+//1.-Crear el elemento
 var caja=document.createElement('div');
+//2.-Crear un nodo de texto
 var contenido=document.createTextNode('Hola Mundo');
+//3.-Añadir el nodo de texto al elemnto
 caja.appendChild(contenido);
+//4.-Agregar atributos a la caja
 caja.setAttribute('class','caja naranja');
+//5.-Agregar el elemento al documento
+//var contenedor =document.getElementById('contenedor');
+//contenedor.appendChild(caja);
 
-var contenedor =document.getElementById('contenedor');
-contenedor.appendChild(caja);
+//Modificando la clase o id de un elemento
+caja.id='primera';
+caja.className='caja naranja';
+
+//Conociendo el elemento padre
+var padre=cajas[0].parentNode;
+//padre.insertBefore(caja,PrimeraCaja);
+//padre.replaceChild(caja,cajas[2]);
+padre.removeChild(caja, cajas[3]);
+
+
